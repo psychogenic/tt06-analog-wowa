01d607e106dbe16ba324c672e8940d2bdb57cb7d
psychogenic <pat.dev@psychogenic.com>
2024-04-17 14:45:56 -0400

    hide options keeps lef write from segfaulting

--- mag/extract.tcl
+++ mag/extract.tcl
@@ -2,6 +2,6 @@ set project tt_um_psychogenic_wowa
 box 0 0 0 0
 load $project.mag
 
-# lef write ../lef/$project.lef -pinonly
+lef write ../lef/$project.lef -pinonly -head
 
 gds write ../gds/$project.gds
