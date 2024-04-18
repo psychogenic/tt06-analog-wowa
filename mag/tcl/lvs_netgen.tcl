29a3223538c971f9fbfcdd6594c7672feefcd5cc
psychogenic <pat.dev@psychogenic.com>
2024-04-18 02:21:57 -0400

    final ground pours to rings, submitting

--- mag/tcl/lvs_netgen.tcl
+++ mag/tcl/lvs_netgen.tcl
@@ -6,5 +6,6 @@ readnet spice $::env(PDK_ROOT)/$::env(PDK)/libs.ref/sky130_fd_sc_hd/spice/sky130
 readnet spice ../xschem/simulation/$project.spice $source
 # readnet spice ../comparator_stefan.spice $source
 # top level GL verilog
+readnet verilog ../src/wowa_digital.v $source
 readnet verilog ../src/project.v $source
 lvs "$layout $project" "$source $project" $::env(PDK_ROOT)/sky130A/libs.tech/netgen/sky130A_setup.tcl lvs.report -blackbox
